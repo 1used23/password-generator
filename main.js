b8000c1092e6c0ec8c1221d2cb8080470fe5c736
1used23 <diziwii@gmail.com>
2019-12-14 04:53:51 +0300

    done

---
+++ main.js
@@ -0,0 +1,80 @@
+// DOM elements
+
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const nubmersEl = document.getElementById("nubmers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+generateEl.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercase.checked;
+  const hasUpper = uppercase.checked;
+  const hasNumbers = numbers.checked;
+  const hasSymbols = symbols.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumbers,
+    hasSymbols,
+    length
+  );
+});
+
+// Password generation
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = "";
+
+  const typesCount = lower + upper + number + symbol;
+
+  if (typesCount === 0) {
+    return "";
+  }
+
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    item => Object.values(item)[0]
+  );
+
+  console.log(typesArr);
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+  return finalPassword;
+}
+
+// Generator functions
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*()-_=+{}[];:"<>,./?|№';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
